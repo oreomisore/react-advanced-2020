ee7ce1a20f0aaa6f6a81af01f9557a75e02d2520
Ore Omisore <ore_omisore@hotmail.com>
2022-04-19 18:45:24 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
+
+// import Final from "./tutorial/1-useState/final/1-error-example";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+// use
+// component name must be uppercase
+// must be in the funciton/component body
+// cannot call conditionally
+
+// useState - name function that we get from react, must have curly braces
+// allows us to track state in a function component.State generally refers to data or properties that need to be tracking in an application.
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello world"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
